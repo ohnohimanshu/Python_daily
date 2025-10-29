1103c57b736e70e10fca6aef9d8add72eccbc04e
Himanshu <himanshuxdei@gmail.com>
2025-10-29 21:07:06 +0530

    fourth question

--- basic.py
+++ basic.py
@@ -6,19 +6,35 @@
 #Take two numbers as input and print their sum.
 
 
-#num = int(input("Enter your first number:"))
-#num2 = int(input("Enter your second number:"))
+'''num = int(input("Enter your first number:"))
+num2 = int(input("Enter your second number:"))
 
-#print("Sum:", num+num2)
-
+print("Sum:", num+num2)
+'''
 
 
 #Write a program to find whether a number is even or odd.
 
-num=int(input("Enter a Num:"))
+'''num=int(input("Enter a Num:"))
 
 if num%2 ==0:
     print("Even ")
 else:
     print("Odd")
+'''
 
+#Write a program to find the largest of three numbers.
+
+'''
+num1 = int(input('Enter first number :'))
+num2 = int(input('Enter second number :'))
+num3 = int(input('Enter third number :'))
+
+if num1 > num2 and num1 > num3:
+    print("Largest number is :", num1)
+elif num2 > num1 and num2 > num3:
+    print("Largest number is :", num2)
+else:
+    print("Largest number is :", num3)
+
+'''
