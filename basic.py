341bd7ae94413dab129a18721fb3fcaa820b3064
Himanshu <himanshuxdei@gmail.com>
2025-10-29 20:56:59 +0530

    second question

--- basic.py
+++ basic.py
@@ -1,3 +1,12 @@
 #Write a Python program to print "Hello, World!"
 
 print("Hello World!")
+
+
+#Take two numbers as input and print their sum.
+
+
+num = int(input("Enter your first number:"))
+num2 = int(input("Enter your second number:"))
+
+print("Sum:", num+num2)
