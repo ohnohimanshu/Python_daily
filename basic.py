d5796512328c38f529cae30ce87fe503000ca932
Himanshu <himanshuxdei@gmail.com>
2025-10-29 20:59:31 +0530

    third question

--- basic.py
+++ basic.py
@@ -1,12 +1,24 @@
 #Write a Python program to print "Hello, World!"
 
-print("Hello World!")
+#print("Hello World!")
 
 
 #Take two numbers as input and print their sum.
 
 
-num = int(input("Enter your first number:"))
-num2 = int(input("Enter your second number:"))
+#num = int(input("Enter your first number:"))
+#num2 = int(input("Enter your second number:"))
 
-print("Sum:", num+num2)
+#print("Sum:", num+num2)
+
+
+
+#Write a program to find whether a number is even or odd.
+
+num=int(input("Enter a Num:"))
+
+if num%2 ==0:
+    print("Even ")
+else:
+    print("Odd")
+
